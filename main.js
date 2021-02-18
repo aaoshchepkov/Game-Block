7d753d53742dc7915d76fde1fae9dcfd4f670e0c
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-02-18 10:46:18 +0700

    урок 14 часть 1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*jshint multistr: true */
 
 function DomElement(selector, height, width, bg, fontSize){
   this.selector = selector;
@@ -15,28 +15,27 @@ DomElement.prototype.addElement = function(){
    let block = document.createElement('div');
    block.classList.add(this.selector.slice(1));
    block.textContent = 'Блок';
-  document.body.prepend(block);
+   document.body.prepend(block);
+   block.style.cssText = 'height: ' + this.height + '; \
+   width: ' + this.width + '; \
+   background-color: ' + this.bg + '; \
+   font-size: '  + this.fontSize;
+
  } else if (this.selector[0] === '#'){
    let parag = document.createElement('p');
    parag.id = this.selector.slice(1);
    parag.textContent = 'Параграф';
    document.body.prepend(parag);
+   parag.style.cssText = 'height: ' + this.height + '; \
+   width: ' + this.width + '; \
+   background-color: ' + this.bg + '; \
+   font-size: '  + this.fontSize;
+
  }
 };
 
-let elementDiv = new DomElement('.block', '40px', '100px', 'red', '20px');
-let elementP = new DomElement('#parag', '40px', '100px', 'tomato', '20px');
-
-
+let elementDiv = new DomElement('.block', '40px', '200px', 'red', '20px');
+let elementP = new DomElement('#parag', '40px', '200px', 'tomato', '20px');
 
 elementDiv.addElement();
-document.querySelector('.block').style.cssText = 'height: ' + elementDiv.height + '; \
-width: ' + elementDiv.width + '; \
-background-color: ' + elementDiv.bg + '; \
-font-size: '  + elementDiv.fontSize;
-
 elementP.addElement();
-document.querySelector('#parag').style.cssText = 'height: ' + elementP.height + '; \
-width: ' + elementP.width + '; \
-background-color: ' + elementP.bg + '; \
-font-size: '  + elementP.fontSize;
